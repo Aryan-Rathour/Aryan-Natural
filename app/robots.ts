0153f49bfc7e18e8e76fa304636d8a5df583b001
abhishek <abhishekmishra06460@gmail.com>
2025-08-24 02:02:04 +0530

    add some other routs in robots file

--- app/robots.ts
+++ app/robots.ts
@@ -5,7 +5,7 @@ export default function robots(): MetadataRoute.Robots {
     rules: {
       userAgent: "*",
       allow: "/",
-      disallow: ["/admin/", "/api/", ],
+      disallow: ["/admin/", "/api/", "/cart/", "/checkout/" ],
     },
     sitemap: "https://rightbasket.in/sitemap.xml",
   }
