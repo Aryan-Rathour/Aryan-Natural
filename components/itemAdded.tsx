f0c3b1aeee8c0cef2490ada50db780e37010e1a9
Aryan rathour <aryanrathour066@gmail.com>
2025-10-09 00:15:25 +0530

    fix: making view cart option over the card not under the card

--- components/itemAdded.tsx
+++ components/itemAdded.tsx
@@ -7,7 +7,7 @@ import Link from "next/link";
 
 const NotificationPopUp = ({ message = "Item added successfully!", onViewCart }) => {
   return (
-<div className="fixed bottom-5 left-1/2 transform -translate-x-1/2 flex items-center justify-between w-[70vw] max-w-md sm:max-w-lg md:max-w-xl bg-green-500 text-white px-4 py-1 rounded shadow-lg">
+<div className="z-50 fixed bottom-5 left-1/2 transform -translate-x-1/2 flex items-center justify-between w-[70vw] max-w-md sm:max-w-lg md:max-w-xl bg-green-500 text-white px-4 py-1 rounded shadow-lg">
       {/* Left side message */}
       <span className="font-medium">{message}</span>
 
